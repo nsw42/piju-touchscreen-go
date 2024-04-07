1d10c128f657357de3e2d411b069387354c95d09
Neil Walker <neil@wynded.co.uk>
2024-04-07 16:54:07 +0100

    Fix quoting issue

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -e
 
 ssh piju "rm -f piju-touchscreen-go"
 scp dist/piju-touchscreen-go piju:
-ssh piju "ps wwaux | grep piju-touchscreen-go | grep -v grep | awk '{print $2}' | xargs kill"
+ssh piju "ps wwaux | grep piju-touchscreen-go | grep -v grep | awk '{print \$2}' | xargs kill"
