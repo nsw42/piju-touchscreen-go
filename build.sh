53371a3f205b649852c7d6e6be8f8214d03f3334
nsw42 <neil@wynded.co.uk>
2025-11-08 10:30:26 +0000

    Update builder docker image

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run -it --rm -v ./:/go/src -v ./dist:/go/output -w /go/src gotk-cross-builder-alpine3.19-armhf sh -c 'go mod tidy; go build -o /go/output/piju-touchscreen-go .'
+docker run -it --rm -v ./:/go/src -v ./dist:/go/output -w /go/src gotk-cross-builder-armhf-alpine-3.22 sh -c 'go mod tidy; go build -o /go/output/piju-touchscreen-go .'
